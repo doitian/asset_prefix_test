8f7f26348bba03a9e24326c9beb5ec1bbcd4d4cc
Ian Yang <me@iany.me>
2013-03-20 05:23:24 +0800

    Add break points in session store class

--- lib/my_cookie_store.rb
+++ lib/my_cookie_store.rb
@@ -1,4 +1,27 @@
 module ActionDispatch::Session
   class MyCookieStore < CookieStore
+    private
+
+    def load_session(env)
+      super.tap do |x|
+        binding.pry
+      end
+    end
+
+    def set_session(*arguments)
+      super.tap do |x|
+        binding.pry
+      end
+    end
+
+    def commit_session?(env, session, options)
+      binding.pry
+      if options[:skip]
+        false
+      else
+        has_session = loaded_session?(session) || forced_session_update?(session, options)
+        has_session && security_matches?(env, options)
+      end
+    end
   end
 end
